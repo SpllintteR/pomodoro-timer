46393e3ba2b26de12edad83f976136319ad56ad8
Ivan Kušt <ivan@infinum.hr>
2015-03-22 23:07:07 +0100

    Added implementation of Trello TokenManager.

--- app/src/main/java/co/ikust/pomodorotimer/rest/oauth/TrelloTokenManager.java
+++ app/src/main/java/co/ikust/pomodorotimer/rest/oauth/TrelloTokenManager.java
@@ -1,6 +1,14 @@
 package co.ikust.pomodorotimer.rest.oauth;
 
+import co.ikust.pomodorotimer.rest.TrelloConstants;
+import co.ikust.pomodorotimer.trello.Trello;
+import co.ikust.pomodorotimer.trello.TrelloDialog;
+import co.ikust.pomodorotimer.trello.TrelloError;
 import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+import static co.ikust.pomodorotimer.PomodoroTimerApplication.getInstance;
 
 /**
  * OAuth {@link co.ikust.pomodorotimer.rest.oauth.TokenManager} for Trello.
@@ -9,18 +17,78 @@ import oauth.signpost.OAuthConsumer;
  */
 public class TrelloTokenManager implements TokenManager {
 
-    @Override
-    public void refreshToken(RefreshTokenCallback callback) {
+    protected final static String KEY_TOKEN = "token";
 
+    protected final static String KEY_TOKEN_SECRET = "token_secret";
+
+    protected static OAuthConsumer createConsumer() {
+        return new CommonsHttpOAuthConsumer(
+                TrelloConstants.APP_KEY,
+                TrelloConstants.APP_SECRET
+        );
+    }
+
+    protected static void storeAccessToken(OAuthConsumer consumer) {
+        getDefaultSharedPreferences(getInstance())
+                .edit()
+                .putString(KEY_TOKEN, consumer.getToken())
+                .putString(KEY_TOKEN_SECRET, consumer.getTokenSecret())
+                .commit();
+    }
+
+    protected static OAuthConsumer loadAccessToken() {
+        OAuthConsumer consumer = createConsumer();
+        consumer.setTokenWithSecret(
+                getDefaultSharedPreferences(getInstance()).getString(KEY_TOKEN, null),
+                getDefaultSharedPreferences(getInstance()).getString(KEY_TOKEN_SECRET, null)
+        );
+
+        return consumer;
+    }
+
+    @Override
+    public void refreshToken(final RefreshTokenCallback callback) {
+        final OAuthConsumer consumer = new CommonsHttpOAuthConsumer(
+                TrelloConstants.APP_KEY,
+                TrelloConstants.APP_SECRET
+        );
+
+        TrelloDialog trelloDialog = new TrelloDialog(getInstance(), consumer, "", new Trello.DialogListener() {
+            @Override
+            public void onComplete(String accessKey, String accessSecret) {
+                storeAccessToken(consumer); //Store new access token.
+
+                if (callback != null) {
+                    callback.onComplete();
+                }
+            }
+
+            @Override
+            public void onError(TrelloError error) {
+                if (callback != null) {
+                    callback.onError(error);
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                if (callback != null) {
+                    callback.onCancel();
+                }
+            }
+        });
+
+        trelloDialog.show();
     }
 
     @Override
     public boolean hasToken() {
-        return false;
+        return getDefaultSharedPreferences(getInstance()).contains(KEY_TOKEN) &&
+                getDefaultSharedPreferences(getInstance()).contains(KEY_TOKEN_SECRET);
     }
 
     @Override
     public OAuthConsumer getToken() {
-        return null;
+        return loadAccessToken();
     }
 }
---
+++ app/src/main/java/co/ikust/pomodorotimer/utils/PreferenceUtils.java
@@ -0,0 +1,9 @@
+package co.ikust.pomodorotimer.utils;
+
+/**
+ * Created by ivan on 22/03/15.
+ */
+public class PreferenceUtils {
+
+
+}
