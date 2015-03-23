78ad05884fb9814ef961a2eee8384e0e97bf3e91
Ivan Kušt <ivan@infinum.hr>
2015-03-23 19:59:31 +0100

    Added REST API call for obtaining list details and associated cards.

--- app/src/main/java/co/ikust/pomodorotimer/rest/RestService.java
+++ app/src/main/java/co/ikust/pomodorotimer/rest/RestService.java
@@ -3,8 +3,10 @@ package co.ikust.pomodorotimer.rest;
 import java.util.ArrayList;
 
 import co.ikust.pomodorotimer.rest.models.Board;
+import co.ikust.pomodorotimer.rest.models.List;
 import retrofit.Callback;
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * Interface that defines REST APIs used in application.
@@ -13,7 +15,14 @@ public interface RestService {
 
 
     @GET("/1/members/me/boards?fields=id,name&lists=all&list_fields=name")
-    void getBoards(Callback<ArrayList<Board>> callback);
+    void getBoards(
+            Callback<ArrayList<Board>> callback
+    );
 
+    @GET("/1/lists/{id}?fields=name&cards=open&card_fields=name")
+    void getList(
+            @Path("id") String listId,
+            Callback<List> callback
+    );
 
 }
--- app/src/main/java/co/ikust/pomodorotimer/rest/models/List.java
+++ app/src/main/java/co/ikust/pomodorotimer/rest/models/List.java
@@ -1,8 +1,15 @@
 package co.ikust.pomodorotimer.rest.models;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by ivan on 23/03/15.
  */
 public class List {
 
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("name")
+    private String name;
 }
